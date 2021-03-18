2969a2847980c2676af734693fe59a12c510fa8e
sarah dankwah <sarahdankwah5@gmail.com>
2021-03-18 12:27:26 +0000

    first init

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+
+
+
+import MyFirstComponent from './MyFirstComponent'
+import MySecondComponent from './MySecondComponent';
+import MyThirdComponent from './MyThirdComponent';
+import MyFourthComponent from './MyFourthComponent';
+import MyFifthComponent from './MyFifthComponent';
+import MySixthComponent from './MySixthComponent';
+import MySeventhComponent from './MySeventhComponent';
+import MyEighthComponent from './MyEighthComponent'
+import MyNinthComponent from './MyNinthComponent'
+import MyTenthComponent from './MyTenthComponent'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <MyFirstComponent/>
+     <MySecondComponent/>
+     <MyThirdComponent/>
+     <MyFourthComponent/>
+     <MyFifthComponent/>
+     <MySixthComponent/>
+     <MySeventhComponent/>
+     <MyEighthComponent/>
+     <MyNinthComponent/>
+     <MyTenthComponent/>
+    </div>
+  );
+}
+
+export default App;
